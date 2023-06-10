f36d9882460f991a13fb6f04319b960962a59251
miguelissa <134017558+miguelissa@users.noreply.github.com>
2023-06-10 11:46:01 -0300

    Practico 1 finalizado

---
+++ Practico1/frutas.js
@@ -0,0 +1,6 @@
+let frutas = [];
+frutas.push("banana", "manzana", "uva", "pera", "melon");
+let ultima= frutas[frutas.length-1];
+console.log("La ultima fruta agregada fue: %s",ultima);
+let primera = frutas[0];
+console.log("La primera fruta agregada fue: %s",primera);
---
+++ Practico1/operacionesAritmeticas.js
@@ -0,0 +1,10 @@
+let var1= 6;
+let var2= 5;
+let var3= 7;
+let var4= 3;
+
+let suma= var1 + var2;
+let resta= var3 - var4;
+let resultadoFinal= suma*resta;
+let esPar= (resultadoFinal % 2) === 0;
+console.log("Mis variables inicales fueron: %i, %i, %i y %i. La repuesta a verificar si el resultado final es par es: %s",var1,var2,var3,var4,esPar);
