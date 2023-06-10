b4923ccaaa32d08e3251536a0a2c401f5a224725
miguelissa <134017558+miguelissa@users.noreply.github.com>
2023-06-10 11:37:05 -0300

    agrego miNombreObjeto.js

---
+++ Practico1/miNombreObjeto.js
@@ -0,0 +1,8 @@
+let nombre;
+nombre = "Miguel";
+let apellido = "Issa";
+let edad = 28;
+
+let persona = {nombre: nombre , apellido: apellido, edad: edad};
+
+console.log("Mi objeto persona es el siguiente: " + JSON.stringify(persona));
