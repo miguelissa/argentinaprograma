18d6ff33f1287b8648e2e6c203cb08b0fd35f6bb
miguelissa <134017558+miguelissa@users.noreply.github.com>
2023-06-24 10:35:07 -0300

    Agrego lanzamiento.js

---
+++ Practico3/lanzamiento.js
@@ -0,0 +1,9 @@
+for (i=10;i>-1;i--){
+    if (i===10){
+        console.log("Cuenta regresiva 10");
+    } else if(i===0){
+        console.log("Lanzamiento!");
+    } else {
+        console.log(i);
+    }
+}
